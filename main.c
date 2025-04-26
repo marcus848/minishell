3cbf2406ef9548e520226f689a0f602b7bc46f0d
Marcus Vinicius Dos Santos Gomes <marcudos@c2r10p1.42sp.org.br>
2025-04-26 16:09:30 -0300

    add tokenizer files

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 20:45:28 by caide-so          #+#    #+#             */
-/*   Updated: 2025/04/23 11:43:08 by caide-so         ###   ########.fr       */
+/*   Updated: 2025/04/25 12:18:05 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	prompt(void);
 void	minishell(char *input);
-
-void	print_env(t_env *env);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -30,17 +28,15 @@ int	main(int argc, char **argv, char **envp)
 }
 
 //TODO: tudo abaixo
-/*
-void	minishell(char *input)
-{
 //	expansao de variavel
-// 	token
 // 	lexer
 // 	parser
 // 	executor
 // 	garbage collector
+void	minishell(char *input)
+{
+	tokenizer(input);
 }
-*/
 
 //TODO: if (validar_input(input)) -- string vazia 
 //TODO: minishell(input);
@@ -60,26 +56,10 @@ void	prompt(void)
 		else if (input != NULL)
 		{
 			add_history(input);
+			minishell(input);
 			free(input);
 		}
 		else
 			printf("Error reading input or end of file reached.\n");
 	}
 }
-
-// ============================ DEBUG FUNCTIONS ===============================
-
-/*
-// Prints the full t_envp struct.
-void	print_env(t_env *env)
-{
-	t_env	*head;
-
-	head = env;
-	while (head)
-	{
-		printf("key = [%s], value = [%s]\n", head->key, head->value);
-		head = head->next;
-	}
-}
-*/
---
+++ tokenizer_handle_operators.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_handle_operators.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 15:44:29 by caide-so          #+#    #+#             */
+/*   Updated: 2025/04/25 15:57:59 by caide-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	is_two_char_op(char *input, t_token_type *type, int *len);
+static int	is_one_char_op_1(char input, t_token_type *type, int *len);
+static int	is_one_char_op_2(char input, t_token_type *type);
+
+int	handle_operators(char *input, int *i, t_token_list *tokens)
+{
+	int				len;
+	char			*value;
+	t_token			*token;
+	t_token_type	type;
+
+	if (!in("()&|<>*", input[*i]))
+		return (0);
+	if (is_two_char_op(input + *i, &type, &len)
+		|| is_one_char_op_1(input[*i], &type, &len))
+	{
+		value = ft_substr(input, *i, len);
+		if (!value)
+			exit_perror("ft_substr");
+		token = new_token(type, value);
+		if (!token)
+		{
+			free(value);
+			exit_perror("new_token");
+		}
+		token_list_append(tokens, token);
+		*i += len;
+		return (1);
+	}
+	return (0);
+}
+
+static int	is_two_char_op(char *input, t_token_type *type, int *len)
+{
+	*len = 2;
+	if (input[0] == '>' && input[1] == '>')
+	{
+		*type = TOKEN_REDIR_APPEND;
+		return (1);
+	}
+	else if (input[0] == '<' && input[1] == '<')
+	{
+		*type = TOKEN_HEREDOC;
+		return (1);
+	}
+	else if (input[0] == '&' && input[1] == '&')
+	{
+		*type = TOKEN_LOGICAL_AND;
+		return (1);
+	}
+	else if (input[0] == '|' && input[1] == '|')
+	{
+		*type = TOKEN_LOGICAL_OR;
+		return (1);
+	}
+	return (0);
+}
+
+static int	is_one_char_op_1(char input, t_token_type *type, int *len)
+{
+	*len = 1;
+	if (input == '>')
+	{
+		*type = TOKEN_REDIR_OUT;
+		return (1);
+	}
+	else if (input == '<')
+	{
+		*type = TOKEN_REDIR_IN;
+		return (1);
+	}
+	else if (input == '|')
+	{
+		*type = TOKEN_PIPE;
+		return (1);
+	}
+	return (is_one_char_op_2(input, type));
+}
+
+static int	is_one_char_op_2(char input, t_token_type *type)
+{
+	if (input == '(')
+	{
+		*type = TOKEN_PAREN_OPEN;
+		return (1);
+	}
+	else if (input == ')')
+	{
+		*type = TOKEN_PAREN_CLOSE;
+		return (1);
+	}
+	else if (input == '*')
+	{
+		*type = TOKEN_ASTERISK;
+		return (1);
+	}
+	return (0);
+}
