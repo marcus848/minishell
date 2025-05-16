f39ae51b4754c0bed0f46625342687f55f20b978
Caio De Souza Marinho <caide-so@c1r9p2.42sp.org.br>
2025-05-15 22:59:56 -0300

    feature: add exit builtin

---
+++ src/builtin/builtin_exit.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_exit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 22:30:29 by caide-so          #+#    #+#             */
+/*   Updated: 2025/05/15 22:55:48 by caide-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+// Check that arg_str consistis only of digits
+// (and optional leading +/-)
+// 	if not, print an error ("numeric argument required") and exit
+// 	with status 2
+int	parse_exit_code(char *arg_str, t_env *env)
+{
+	int	code;
+	int	i;
+
+	if (!arg_str)
+		return (get_last_status(env));
+	i = 0;
+	if (arg_str[i] == '+' || arg_str[i] == '-')
+		i++;
+	while (arg_str[i])
+	{
+		if (!ft_isdigit(arg_str[i]))
+		{
+			ft_putstr_fd("exit\n", STDERR_FILENO);
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(arg_str, STDERR_FILENO);
+			ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+			exit(2);
+		}
+		i++;
+	}
+	code = ft_atoi(arg_str);
+	return (code & 0xFF);
+}
