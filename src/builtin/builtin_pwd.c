30529bc137956d3796cb41c8139bb5f72e53a8ca
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2025-05-19 02:31:46 -0300

    refactor: use return instead of exit

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 01:57:27 by caide-so          #+#    #+#             */
-/*   Updated: 2025/05/19 02:17:21 by caide-so         ###   ########.fr       */
+/*   Updated: 2025/05/19 02:31:12 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	builtin_pwd(void)
 
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
-		exit_perror("pwd");
+	{
+		perror("pwd");
+		return (1);
+	}
 	printf("%s\n", pwd);
 	free(pwd);
 	return (0);
