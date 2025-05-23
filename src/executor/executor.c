8db08c39e4b32b34e81d2365d479b442cf48de83
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2025-05-22 22:40:53 -0300

    fix: norminette

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -14,48 +14,52 @@
 
 void	exec_command(t_token_list *tokens, t_ast *node, t_env *env);
 
+/*
+if (node->type == NODE_COMMAND)
+{
+	// variable expansion
+	// wildcard expansion
+ 	// handle redirections
+ 	// execute builtin or fork-exec
+ 	// restore redirections
+	expander(&node->cmd->args, env, &node->cmd->arg_count);
+	exec_command(tokens, node, env);
+}
+if (node->type == NODE_PIPE)
+	// create pipe
+	// fork left, redirect stdout
+	// fork right, redirect stdin
+	// wait and update status
+	// obs: inside exec_pipe fork left/right and maybe recurse there
+	exec_pipe(node, env);
+if (node->type == NODE_AND)
+{
+	// execute left, test status, maybe execute right
+	executor(node->left, env);
+	if (get_last_status(env) == 0)
+		executor(node->right, env);
+}
+if (node->type == NODE_OR)
+{
+	// execute left, test status, maybe execute right
+	executor(node->left, env);
+	if (get_last_status(env) != 0)
+		executor(node->right, env);
+}
+if (node->type == NODE_SUBSHELL)
+	// fork, apply redirs, exec subtree, wait and update status
+	// obs: subshell must execute in a child process
+	executor(node->left, env);
+*/
 void	executor(t_token_list *tokens, t_ast *node, t_env *env)
 {
 	if (!node)
 		return ;
-	// traverse ast
 	if (node->type == NODE_COMMAND)
 	{
-		// wildcard expansion
-		// variable expansion
- 		// handle redirections
- 		// execute builtin or fork-exec
- 		// restore redirections
 		expander(&node->cmd->args, env, &node->cmd->arg_count);
 		exec_command(tokens, node, env);
 	}
-	/*
-	if (node->type == NODE_PIPE)
-		// create pipe
-		// fork left, redirect stdout
-		// fork right, redirect stdin
-		// wait and update status
-		// obs: inside exec_pipe fork left/right and maybe recurse there
-		exec_pipe(node, env);
-	if (node->type == NODE_AND)
-	{
-	 	// execute left, test status, maybe execute right
-		executor(node->left, env);
-		if (get_last_status(env) == 0)
-			executor(node->right, env);
-	}
-	if (node->type == NODE_OR)
-	{
-	 	// execute left, test status, maybe execute right
-		executor(node->left, env);
-		if (get_last_status(env) != 0)
-			executor(node->right, env);
-	}
-	if (node->type == NODE_SUBSHELL)
-		// fork, apply redirs, exec subtree, wait and update status
-		// obs: subshell must execute in a child process
-		executor(node->left, env);
-	*/
 	return ;
 }
 
