1ee30848720b9fd493600ce1b9ef1b56e0511aab
marcusgomes <72327967+marcus848@users.noreply.github.com>
2025-06-06 22:34:44 -0300

    Improve heredoc SIGINT handling

--- src/signal/setup_signals_heredoc.c
+++ src/signal/setup_signals_heredoc.c
@@ -12,10 +12,16 @@
 
 #include "../../include/minishell.h"
 
-// void	setup_signals_heredoc(void)
-// {
-//
-// }
+void    setup_signals_heredoc(void)
+{
+        t_sig   sa_int;
+
+        sa_int.sa_handler = handle_sigint_heredoc;
+        sigemptyset(&sa_int.sa_mask);
+        sa_int.sa_flags = 0;
+        sigaction(SIGINT, &sa_int, NULL);
+        signal(SIGQUIT, SIG_IGN);
+}
 
 void	handle_sigeof_heredoc(char *delim)
 {
@@ -25,8 +31,12 @@ void	handle_sigeof_heredoc(char *delim)
 	write(1, "')\n", 3);
 }
 
-void	handle_sigint_heredoc(int sig)
+void    handle_sigint_heredoc(int sig)
 {
-	(void) sig;
-	g_signal_status = 130;
+        (void) sig;
+        g_signal_status = 130;
+        write(1, "\n", 1);
+        rl_replace_line("", 0);
+        rl_on_new_line();
+        rl_redisplay();
 }
